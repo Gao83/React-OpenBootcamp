c11a72e54305d07af4c0020c4876f42ba02e5204
Isaac Miralles <isaacmiralles@gmail.com>
2022-08-04 14:33:40 +0200

    observable

---
+++ src/components/pure/ObservableExample.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ObservableExample() {
+    return (
+        <div>
+            <h1>Observable Exampleg</h1>
+        </div>
+    )
+}
+
+export default ObservableExample
